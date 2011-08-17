ca32ee870dfc3c13426eb5b1fd5fe15ae9ce0e33
Lloyd Pick <lloydpick@gmail.com>
2011-08-17 16:48:53 +0100

    change to the current_path before running commands

--- lib/resque_utils/capistrano.rb
+++ lib/resque_utils/capistrano.rb
@@ -8,20 +8,20 @@ Capistrano::Configuration.instance(:must_exist).load do
   
       desc "Requeue all failed jobs"
       task :requeue_all do
-        set :resque_pre, "#{current_path} && RAILS_ENV=#{environment} bundle exec rails runner"
+        set :resque_pre, "cd #{current_path} && RAILS_ENV=#{environment} bundle exec rails runner"
         run "#{resque_pre} 'ResqueUtils::requeue_all'"
       end
   
       desc "Remove all failed jobs"
       task :remove_all do
-        set :resque_pre, "#{current_path} && RAILS_ENV=#{environment} bundle exec rails runner"
+        set :resque_pre, "cd #{current_path} && RAILS_ENV=#{environment} bundle exec rails runner"
         run "#{resque_pre} 'ResqueUtils::remove_all'"
       end
   
       desc "Requeue specific failed jobs (specify with '--set exception=SomeErrorHere)"
       task :requeue_specific do
         if exists?(:exception)
-          set :resque_pre, "#{current_path} && RAILS_ENV=#{environment} bundle exec rails runner"
+          set :resque_pre, "cd #{current_path} && RAILS_ENV=#{environment} bundle exec rails runner"
           run "#{resque_pre} \"ResqueUtils::requeue_all('#{exception}')\""
         else
           raise ArgumentError, "No exception was specified, use '--set exception=SomeErrorHere'"
@@ -31,7 +31,7 @@ Capistrano::Configuration.instance(:must_exist).load do
       desc "Remove specific failed jobs (specify with '--set exception=SomeErrorHere)"
       task :remove_specific do
         if exists?(:exception)
-          set :resque_pre, "#{current_path} && RAILS_ENV=#{environment} bundle exec rails runner"
+          set :resque_pre, "cd #{current_path} && RAILS_ENV=#{environment} bundle exec rails runner"
           run "#{resque_pre} \"ResqueUtils::remove_all('#{exception}')\""
         else
           raise ArgumentError, "No exception was specified, use '--set exception=SomeErrorHere'"
